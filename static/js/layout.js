290048aeb6bafebc08121e2ba3efb6e62c590b32
ferchirinoboss <chirinoboss@gmail.com>
2025-04-26 21:08:46 -0600

    Enhanced sidebar functionality with submenu toggle and updated layout structure

--- static/js/layout.js
+++ static/js/layout.js
@@ -3,3 +3,7 @@ const sidebar = document.getElementById("sidebar");
 function toggleSidebar() {
     sidebar.classList.toggle("show")
 }
+
+function toggleSubMenu(button){
+    button.nextElementSibling.classList.toggle("show")
+}
